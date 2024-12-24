010fa7064a42f5169d07cbee9d5753a051f27afe
BetDC <beto@MacBook-Pro-de-Beto.local>
2024-12-23 21:04:01 -0600

    try to fix this

--- web_flask/10-hbnb_filters.py
+++ web_flask/10-hbnb_filters.py
@@ -1,31 +1,30 @@
 #!/usr/bin/python3
-"""Starts a Flask web application.
-
-The application listens on 0.0.0.0, port 5000.
-Routes:
-    /hbnb_filters: HBnB HTML filters page.
 """
+Script that starts a Flask web app.
+"""
+
 from models import storage
-from flask import Flask
-from flask import render_template
+from flask import render_template, Flask
+from models.state import State
+from models.amenity import Amenity
 
 app = Flask(__name__)
 
 
-@app.route("/hbnb_filters", strict_slashes=False)
-def hbnb_filters():
-    """Displays the main HBnB filters HTML page."""
-    states = storage.all("State")
-    amenities = storage.all("Amenity")
-    return render_template("10-hbnb_filters.html",
-                           states=states, amenities=amenities)
-
-
 @app.teardown_appcontext
-def teardown(exc):
-    """Remove the current SQLAlchemy session."""
+def remove_sqlsession(exception=None):
+    """A method that removes SQL Session"""
     storage.close()
 
 
+@app.route("/hbnb_filters", strict_slashes=False)
+def hbnb_filters():
+    """Display HBNB filters"""
+    list_states = storage.all(State)
+    list_amenity = storage.all(Amenity)
+    return render_template("10-hbnb_filters.html", states=list_states,
+                           amenities=list_amenity)
+
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+    app.run(host='0.0.0.0', port=5000)
